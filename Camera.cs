b67467920d888ab93d9d8a1ee69c4bb1fbcac062
b0ry <chuckborisnorris@mail.com>
2016-04-06 19:26:39 +0100

    Added files via upload

---
+++ Camera.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+	GUITexture bgTexture;
+	WebCamTexture camTexture;
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Initialize");
+		bgTexture = gameObject.AddComponent<GUITexture>();
+		bgTexture.pixelInset = new Rect(0,0,Screen.width,Screen.height);
+		//set up camera
+		WebCamDevice[] devices = WebCamTexture.devices;
+		string backCamName="";
+		for( int i = 0 ; i < devices.Length ; i++ ) {
+			Debug.Log("Device:"+devices[i].name+ "IS FRONT FACING:"+devices[i].isFrontFacing);
+			
+			if (devices[i].isFrontFacing) {
+				backCamName = devices[i].name;
+			}
+		}
+		
+		camTexture = new WebCamTexture(backCamName,10000,10000,30);
+		camTexture.Play();
+		bgTexture.texture = camTexture;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
